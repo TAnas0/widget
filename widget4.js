8386a43483c2c439b256bb5a1ad3508cdf85f77b
anas5 <anastiour@gmail.com>
2020-01-17 01:28:26 +0000

    copy widget2 to refresh CDN

---
+++ widget4.js
@@ -0,0 +1,67 @@
+// and here's the trick (works everywhere)
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+// use like
+r(function(){
+  // alert('DOM Ready!')
+  console.log('Hey from inside a script tag')
+  console.log(Shopify)
+
+  var elem = document.createElement('div');
+  elem.setAttribute('id', 'sb_widget')
+  document.body.appendChild(elem)
+
+  const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+  const numBalls = 50;
+  var balls = [];
+  for (let i = 0; i < numBalls; i++) {
+    let ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    ball.style.transform = `scale(${Math.random()})`;
+    ball.style.width = `${Math.random()}em`;
+    ball.style.height = ball.style.width;
+
+    balls.push(ball);
+    document.body.append(ball);
+  }
+
+
+  // Keyframes
+  balls.forEach((el, i, ra) => {
+    let to = {
+      x: Math.random() * (i % 2 === 0 ? -11 : 11),
+      y: Math.random() * 12
+    };
+
+    let anim = el.animate(
+      [
+        { transform: "translate(0, 0)" },
+        { transform: `translate(${to.x}rem, ${to.y}rem)` }
+      ],
+      {
+        duration: (Math.random() + 1) * 2000, // random duration
+        direction: "alternate",
+        fill: "both",
+        iterations: Infinity,
+        easing: "ease-in-out"
+      }
+    );
+  });
+
+
+  var ballDivs = document.getElementsByClassName('ball')
+
+  ballDivs.foEach((el, i ,ra) => {
+    el.setAttribute('position', 'absolute')
+    el.setAttribute('border-radius', '100%')
+    el.setAttribute('opacity', '0.7')
+  })
+
+  console.log('End of script tag')
+});
+
+
+
+
